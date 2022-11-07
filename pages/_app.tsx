e125beca2cf9dfa0d428918d6cc3955b8c8c2ae8
Marty Wallace <marty@martywallace.com>
2022-11-08 08:36:57 +1100

    Add GA.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Script from 'next/script';
 import Footer from '../components/Footer';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Marty Wallace | Full Stack Developer</title>
+      </Head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=UA-16827384-5"
+      />
+      <Script strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-16827384-5');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Footer />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import About from '../components/Sections/About';
 import Introduction from '../components/Sections/Introduction';
 import WorkWithMe from '../components/Sections/Work';
@@ -7,9 +6,6 @@ import WorkWithMe from '../components/Sections/Work';
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Marty Wallace | Full Stack Developer</title>
-      </Head>
       <Introduction />
       <About />
       {/* <Articles /> */}
