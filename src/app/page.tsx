11919923512edafd19695491975712e89bf9030f
Marty Wallace <marty@martywallace.com>
2023-12-14 07:54:16 +1100

    remove: clients section for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import About from '../components/Sections/About';
 import Articles from '../components/Sections/Articles';
 import History from '../components/Sections/History';
 import Introduction from '../components/Sections/Introduction';
-import Clients from '../components/Sections/Clients';
 
 export default function Home() {
   return (
     <>
       <Introduction />
       <About />
-      <Clients />
+      {/*<Clients />*/}
       <History />
       <Articles />
     </>
